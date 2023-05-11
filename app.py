0367566acb7bf875b422325579ac8ae16a8d7cb4
PotturiManikanta <68027205+PotturiManikanta@users.noreply.github.com>
2023-05-11 15:11:05 +0000

    Template and API Route

--- app.py
+++ app.py
@@ -3,10 +3,40 @@ from flask import *
 app = Flask(__name__)
 
 
+JOBS=[
+  {
+  'id':1,
+  'title':'Java',
+  'location':'Online',
+  'salary':'Rs.10,000'
+},
+{
+  'id':2,
+  'title':'Python',
+  'location':'Offline',
+  'salary':'Rs.20,000'
+},
+{
+  'id':3,
+  'title':'Fullstack',
+  'location':'Online',
+  'salary':'Rs.70,000'
+},
+{
+  'id':4,
+  'title':'Datastructures',
+  'location':'Online',
+  'salary':'Rs.50,000'
+}
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS,company_name='Manikanta')
 
 
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
